0b5d75a29c6c9db3becb0634903c69d5496d21c5
Yufan Sheng <syhily@gmail.com>
2026-05-09 15:57:16 +0800

    chore: remove the deprecated config for tsdown.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
     entry: ['./src/main.ts'],
-    skipNodeModulesBundle: true,
-    format: ['cjs', 'esm']
+    format: ['cjs', 'esm'],
+    deps: {
+        skipNodeModulesBundle: true,
+    },
 })
